7787b0f8de2c0287cbac2ab20589945b07a019d5
wanish sengkhim <wanish@sirisoft.co.th>
2023-07-02 13:28:09 +0700

    first commit

---
+++ src/main/java/estrolldomezx/com/example/googlecloudstorage/GoogleCloudStorageCrudApplication.java
@@ -0,0 +1,13 @@
+package estrolldomezx.com.example.googlecloudstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCloudStorageCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCloudStorageCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/estrolldomezx/com/example/googlecloudstorage/GoogleCloudStorageCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package estrolldomezx.com.example.googlecloudstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleCloudStorageCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
